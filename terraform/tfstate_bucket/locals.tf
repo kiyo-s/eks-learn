d9facb8c7d386320af8e1bb30ad84fcc45b4f18d
Kyohei.Saito <kyohei.saito@3-shake.com>
2024-09-30 22:35:36 +0900

    初期ファイルとしてプロバイダーや共通変数を追加した

---
+++ terraform/tfstate_bucket/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  state     = "tfstate"
+  managedby = "terraform"
+  git_repos = "https://github.com/kiyo-s/eks-learn.git"
+  name      = "eks-learn-${var.env}-${local.state}"
+
+  default_tags = {
+    Env           = var.env
+    State         = local.state
+    ManagedBy     = local.managedby
+    GitRepository = local.git_repos
+    Name          = local.name
+  }
+}
