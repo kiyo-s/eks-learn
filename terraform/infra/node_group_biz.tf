895701a2b30602a61916fcea1f452ed0224e8511
Kyohei.Saito <kyohei.saito@3-shake.com>
2024-10-14 21:49:47 +0900

    業務アプリケーション用のノードグループのために system をコピーした

---
+++ terraform/infra/node_group_biz.tf
@@ -0,0 +1,48 @@
+module "eks_node_group_system" {
+  source = "./modules/managed_nodegroup"
+
+  resource_name_prefix = "${local.name}-node-group-system"
+  node_group_name      = "system"
+  tags                 = local.default_tags
+
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = values(aws_subnet.private)[*].id
+
+  eks_cluster_name              = aws_eks_cluster.main.name
+  cluster_endpoint              = aws_eks_cluster.main.endpoint
+  cluster_ca                    = aws_eks_cluster.main.certificate_authority[0].data
+  cluster_cidr                  = aws_eks_cluster.main.kubernetes_network_config[0].service_ipv4_cidr
+  eks_cluster_security_group_id = aws_security_group.eks_cluster.id
+
+  ami_id = var.ami_id
+
+  node_resources = var.node_resources_system
+
+  k8s_node_labels = {
+    type = "system"
+  }
+
+  max_unavailable_percentage = var.max_unavailable_percentage
+
+  is_enabled_cluster_autoscaler = true
+}
+
+data "aws_autoscaling_group" "eks_node_group_system" {
+  name = module.eks_node_group_system.autoscaling_group_name[0]
+}
+
+resource "aws_autoscaling_group_tag" "eks_node_group_system_cluster_autoscaler" {
+  for_each = {
+    "k8s.io/cluster-autoscaler/enabled" : "true",
+    "k8s.io/cluster-autoscaler/${aws_eks_cluster.main.name}" : "owned",
+  }
+
+  autoscaling_group_name = data.aws_autoscaling_group.eks_node_group_system.name
+
+  tag {
+    key   = each.key
+    value = each.value
+
+    propagate_at_launch = false
+  }
+}
